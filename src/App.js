f8338332ad961154a02ffe504be316d79ddd6728
subhaprasad jena <jenasubhaprasad@gmail.com>
2022-03-27 10:07:13 +0530

    2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { Header } from './Components/HeaderC/Header';
+import SimpleBottomNavigation from './Components/Nav/Navbar';
+import {BrowserRouter as Router,Route,Routes } from "react-router-dom"
+import { Container} from '@material-ui/core';
+import Trending from './Components/Pages/Trending/Trending';
+import Movies from './Components/Pages/Movie/Movies';
+import Search from './Components/Pages/Search/search';
+import Series from './Components/Pages/Series/Series';
 function App() {
   return (
-    <div className="App">
-      gpogjerijierjgieiop
-    </div>
+    <Router>
+    <Header/>
+  <div className="app">
+    <Container>
+      <Routes>
+        <Route path='/' element={<Trending/>} exact/>
+        <Route path='/movies' element={<Movies/>}/>
+        <Route path='/series' element={<Series/>}/>
+        <Route path='/search' element={<Search/>}/>
+        
+      </Routes>
+    </Container>
+  
+  </div>
+  <SimpleBottomNavigation/>
+    
+  </Router>
+  
   );
 }
 
---
+++ src/Components/Pages/Series/Series.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Series = () => {
+  return (
+    <div><span>Series.........</span></div>
+  )
+}
+
+export default Series
